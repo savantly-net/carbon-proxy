667aeeb902ab0c0a65aac434d84105fa46e4ad21
Jeremy <jdbranham@hotmail.com>
2017-11-20 13:01:45 -0600

    refactored, added performance monitoring, removed hawtio

--- src/main/java/net/savantly/metrics/carbonProxy/Application.java
+++ src/main/java/net/savantly/metrics/carbonProxy/Application.java
@@ -6,19 +6,12 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.hawt.springboot.EnableHawtio;
-import io.hawt.web.AuthenticationFilter;
-
-@EnableHawtio
 @SpringBootApplication
 public class Application implements CommandLineRunner {
 
     private final static Logger log = LoggerFactory.getLogger(Application.class);
     
     public static void main(String[] args) {
-    	if(!System.getProperties().containsKey(AuthenticationFilter.HAWTIO_AUTHENTICATION_ENABLED)){
-    		System.setProperty(AuthenticationFilter.HAWTIO_AUTHENTICATION_ENABLED, "false");
-    	}
         SpringApplication.run(Application.class, args);
     }
 
---
+++ src/main/java/net/savantly/metrics/carbonProxy/PerformanceMonitoringConfiguration.java
@@ -0,0 +1,60 @@
+package net.savantly.metrics.carbonProxy;
+
+import java.util.Map;
+
+import javax.validation.constraints.Min;
+
+import org.joda.time.DateTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.actuate.endpoint.MetricsEndpoint;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.channel.QueueChannel;
+import org.springframework.messaging.support.GenericMessage;
+import org.springframework.scheduling.annotation.Scheduled;
+
+@Configuration
+@ConfigurationProperties("carbon.performance")
+public class PerformanceMonitoringConfiguration {
+	private final Logger log = LoggerFactory.getLogger(PerformanceMonitoringConfiguration.class);
+	private boolean monitoring = true;
+	private String prefix = "";
+	// TODO: allow metric exclusion 
+	//private String[] exclude = new String[0];
+	
+	@Min(value=1000, message="the rate should be at least 1 second")
+	private int frequencyMs = 60000;
+	
+	private QueueChannel carbonQueue;
+	private MetricsEndpoint metricsEndpoint;
+	
+	@Autowired
+	public PerformanceMonitoringConfiguration(
+			@Qualifier("carbonQueue") QueueChannel carbonQueue,
+			MetricsEndpoint metricsEndpoint) {
+		this.carbonQueue = carbonQueue;
+		this.metricsEndpoint = metricsEndpoint;
+	}
+	
+	@Scheduled(fixedRateString="${carbon.performance.frequencyMs:60000}")
+	public void sendStatistics() {
+		long epoch = DateTime.now().getMillis()/1000;
+		Map<String, Object> response = metricsEndpoint.invoke();
+		response.keySet().stream().forEach((k) -> {
+			String metricString = String.format("%s%s %s %s\n", this.prefix, k, response.get(k), epoch);
+			log.debug("sending performance metrics: {}", metricString);
+			this.carbonQueue.send(new GenericMessage<String>(metricString));
+		});
+	}
+
+	public boolean isMonitoring() {
+		return monitoring;
+	}
+
+	public void setMonitoring(boolean monitoring) {
+		this.monitoring = monitoring;
+	}
+}
