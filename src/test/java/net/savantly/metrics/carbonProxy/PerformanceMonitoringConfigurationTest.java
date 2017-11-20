7e6a57360ab7edc13c35eda88e6d4436d9fe2339
Jeremy <jdbranham@hotmail.com>
2017-11-20 13:05:02 -0600

    dirties context

--- src/test/java/net/savantly/metrics/carbonProxy/PerformanceMonitoringConfigurationTest.java
+++ src/test/java/net/savantly/metrics/carbonProxy/PerformanceMonitoringConfigurationTest.java
@@ -4,10 +4,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.MethodMode;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@DirtiesContext(methodMode=MethodMode.AFTER_METHOD)
 public class PerformanceMonitoringConfigurationTest {
 	
 	@Autowired
